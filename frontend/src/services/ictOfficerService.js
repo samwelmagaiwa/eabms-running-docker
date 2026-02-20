919740be33967e974e9cc4318c9da13622eb263b
samwelmagaiwa <samwelmagaiwa229@gmail.com>
2026-02-20 15:56:36 +0300

    fix: standardize frontend api base urls to relative paths

--- frontend/src/services/ictOfficerService.js
+++ frontend/src/services/ictOfficerService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { devLog } from '../utils/devLogger'
 
-const API_BASE_URL = process.env.VUE_APP_API_URL || 'http://127.0.0.1:8000/api'
+const API_BASE_URL = process.env.VUE_APP_API_URL || '/api'
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
